8d60cc94a9b6aa00c5a41e0aef28bd48b6ec70d3
Edoardo G <edoardogalantini@yahoo.it>
2025-07-16 03:12:55 +0200

    implemented improvements

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,21 +1,12 @@
 import 'package:go_router/go_router.dart';
 import '../features/planner/planner_screen.dart';
-import '../features/recipes/recipes_screen.dart';
 import '../features/favorites/favorites_screen.dart';
 import '../features/shopping_list/shopping_list_screen.dart';
-import '../features/settings/settings_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const PlannerScreen(),
-    ),
-    GoRoute(
-      path: '/recipes',
-      builder: (context, state) => const RecipesScreen(),
-    ),
+    GoRoute(path: '/', builder: (context, state) => const PlannerScreen()),
     GoRoute(
       path: '/favorites',
       builder: (context, state) => const FavoritesScreen(),
@@ -23,10 +14,6 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/shopping',
       builder: (context, state) => const ShoppingListScreen(),
-    ),
-    GoRoute(
-      path: '/settings',
-      builder: (context, state) => const SettingsScreen(),
     ),
   ],
 );
